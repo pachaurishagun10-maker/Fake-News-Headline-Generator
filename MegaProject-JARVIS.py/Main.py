d82e902d2b4b3431b44d73aebefbe12b179a79bb
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-08-26 12:14:24 +0530

    Updated code

--- MegaProject-JARVIS.py/Main.py
+++ MegaProject-JARVIS.py/Main.py
@@ -16,11 +16,7 @@ def speak(text):
     engine.runAndWait()
 
 def aiProcess(command):
-    client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
-
-def aiProcess(command):
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
     completion = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
@@ -71,6 +67,9 @@ def processCommand(c):
             for article in articles:
                 speak(article['title'])          
 
+    else:
+        output=aiProcess(c)
+        speak(output)
     
 if __name__ == "__main__":
     speak("Activating Jarvis")
