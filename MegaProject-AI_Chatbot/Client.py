de291b340a521ce017e431bdf381e00839d05169
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-08-29 16:37:58 +0530

    Updated commands

--- MegaProject-AI_Chatbot/Client.py
+++ MegaProject-AI_Chatbot/Client.py
@@ -7,18 +7,22 @@ client=Groq(
     api_key=os.getenv("GROQ_API_KEY")
 )
 
-def aiProcess(command):
+command='''
+[19:30 , 10/5/2025] rio:Hello! kaisa hai?
+[19:30 , 10/5/2025] juhu:Badhiya tu bata kaise yaad kia
+[19:30 , 10/5/2025] rio:I'm also good,Mujhe kuch books suggest kar as a beginner
+[19:30 , 10/5/2025] juhu:Achaa!!Try reading , The Alchemist,Subtle Art of not giving a f*ck,Gone Girl 
+[19:30 , 10/5/2025] rio:Acha great yaar, i'm trying to build reading habit
+[19:30 , 10/5/2025] juhu:Great!! That's really good let me know kaisi lagi, I'd love to listen
+[19:30 , 10/5/2025] rio:sure!!Thank you so much
+[19:30 , 10/5/2025] juhu:No worries!!Most Welcome.
+'''
     completion = client.chat.completions.create(
         model="openai/gpt-oss-20b",
         messages=[
-            {
-                "role": "system",
-                "content": "You are a virtual assistant named Jarvis skilled in general tasks like Alexa and Google Cloud."
-            },
-            {
-                "role": "user",
-                "content": command
-            }
+            {"role": "system","content": "You are a person named Juhu who speaks hindi and english.He is from India and is a engineer.You analyze chat history and respond like Juhu"},
+            
+            {"role": "user","content":Command}
         ]
     )
 
