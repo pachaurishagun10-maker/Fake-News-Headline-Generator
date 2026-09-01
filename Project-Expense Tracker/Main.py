1ad625308d8c3c65b7b6627f4ae627e01d6f3a85
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-09-01 17:42:35 +0530

    Updated code

--- Project-Expense Tracker/Main.py
+++ Project-Expense Tracker/Main.py
@@ -25,3 +25,41 @@ class ExpenseTracker:
             print("Expense list:")
             for i,self.expense in enumerate(self.expenses,start=1):
                 print(f"{i}. Date: {self.expense.date}, Description: {self.expense.description}, Amount: {self.expense.amount}")
+
+    def total_expenses(self):
+        total=sum(expense.amount for expense in self.expenses)
+        print(f"Total Expenses: Rs. {total:.2f}") 
+
+    def main():
+        tracker=ExpenseTracker()
+
+        while True:
+            print("\nExpense Tracker Menu:")
+            print("1. Add Expense")
+            print("2. Remove Expense")
+            print("3. View Expenses")
+            print("4. Total Expenses")
+            print("5. Exit")    
+
+            choice=input("Enter your choice (1-5): ")
+
+            if choice=="1":
+                date=input("Enter the date (YYYY-MM-DD): ")
+                description=input("Enter the description: ")
+                amount=float(input("Enter the amount: "))
+                expense=Expense(date,description,amount)
+                tracker.add_expense(expense)
+                print("Expense added successfully.")
+
+            elif choice=="2":
+                index=int(input("Enter the index of the expense to remove: "))
+                tracker.remove_expense(index-1)
+            elif choice=="3":
+                tracker.view_expenses()
+            elif choice=="4":
+                tracker.total_expenses()
+            elif choice=="5":
+                print("Exiting Expense Tracker!!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
