37c3d9c080be43c901ffd4d0681d68b3c4718ec5
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-09-03 22:23:57 +0530

    Debugged Code

--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
 import random
-subjects = ["Akshay kumar","M.S.Dhoni","A naughty monkey","A cab driver from Mumbai","A dancing cat"]
-actions = ["eats","sings","plays","studies","cry"]
-places = ["In filmcity","In the stadium","at Amer fort","at ganga ghat","in the jungle"]
+
+subjects = ["Akshay kumar", "M.S.Dhoni", "A naughty monkey", 
+            "A cab driver from Mumbai", "A dancing cat"]
+
+actions = ["eats", "sings", "plays", "studies", "cries"]
+
+places = ["in filmcity", "in the stadium", "at Amer fort", 
+          "at ganga ghat", "in the jungle"]
 
 while True:
-    subject=random.choice(subjects)
-    actions=random.choice(actions)
-    places=random.choice(places)
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place = random.choice(places)
 
-    headline=f"BREAKING NEWS: {subject} {actions} {places}"
+    headline = f"BREAKING NEWS: {subject} {action} {place}"
     print("\n" + headline)
 
-    user_input=input("\nDo you want another headline? (yes/no)").strip().lower()
+    user_input = input("\nDo you want another headline? (yes/no) ").strip().lower()
+
     if user_input != "yes":
         break
 
-print("\nThank you for using the Fake News Headline Generator.Have a fun day!")
+print("\nThank you for using the Fake News Headline Generator. Have a fun day!")
