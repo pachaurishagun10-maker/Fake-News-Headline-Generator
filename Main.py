d152ab377a0ba862761f3cdfbf7a2139f030de6d
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-09-03 21:40:47 +0530

    Updated lists

---
+++ Main.py
@@ -0,0 +1,4 @@
+import random
+subjects = ["Akshay kumar","M.S.Dhoni","A naughty monkey","A cab driver from Mumbai","A dancing cat"]
+actions = ["eats","sings","plays","studies","cry"]
+places = ["In filmcity","In the stadium","at Amer fort","at ganga ghat","in the jungle"]
